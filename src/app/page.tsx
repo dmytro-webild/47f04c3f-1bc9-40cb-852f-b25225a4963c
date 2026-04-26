798af691857c3daa9ad485f648e78b99c057515b
bender <kudin.dima@gmail.com>
2026-04-26 12:55:00 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,9 +121,9 @@ export default function LandingPage() {
         {
           id: "p3",          name: "ZenCortex",          price: "$29.99",          variant: "New Product",          imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777207935522-5rsajsen.png"},
         {
-          id: "p4",          name: "Mind Armor",          price: "$45.99",          variant: "60 Tablets",          imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777207953113-bixsk843.png"},
+          id: "p4",          name: "Mind Armor",          price: " ",          variant: " ",          imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777207953113-bixsk843.png"},
         {
-          id: "p5",          name: "Mindful Matcha",          price: "$19.99",          variant: "Standard Pack",          imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/supplement-bottle-focusing-on-focus-clea-1777201378347-11370fc7.png"},
+          id: "p5",          name: "Mindful Matcha",          price: "$19.99",          variant: "Standard Pack",          imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777208017725-fyrtckxc.png"},
         {
           id: "p6",          name: "Daily Calm Routine",          price: "$59.99",          variant: "3-Piece Set",          imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/wellness-product-peaceful-aura-elegant-c-1777201386356-7e54b894.png"},
       ]}
