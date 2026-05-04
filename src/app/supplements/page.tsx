ac431f0153a25ce2388559d1f628b539d7bee218
bender <kudin.dima@gmail.com>
2026-05-04 06:56:40 +0000

    Update src/app/supplements/page.tsx

--- src/app/supplements/page.tsx
+++ src/app/supplements/page.tsx
@@ -42,16 +42,16 @@ export default function SupplementsPage() {
             animationType="slide-up"
             useInvertedBackground={false}
             products={[
-              { id: "s1", name: "Memory Fuel", price: "", variant: "Selling Fast", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777868743731-y4wa2vn3.png?_wi=1" },
-              { id: "s2", name: "Mind Armor", price: "", variant: "Selling Fast", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777869055586-ikhqg5on.png?_wi=1" },
-              { id: "s3", name: "ZenCortex", price: "", variant: "New Product", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777869239141-ymqry49a.png?_wi=1" },
-              { id: "s4", name: "Java Brain", price: "", variant: "New Product ", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777876557877-sxo8ck2a.png" },
-              { id: "s5", name: "Brain Song", price: "", variant: "Selling Fast", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777873454182-yswjwhig.png" },
-              { id: "s6", name: "Brain's Potential", price: "", variant: "Selling Fast", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875638563-frz4xls5.png" },
-              { id: "s7", name: "Neuro Prime", price: "", variant: "Selling Fast", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777874864346-8ojlmuxm.png" },
-              { id: "s8", name: "Pineal Guardian X", price: "", variant: "Selling Fast", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777874884898-6h39qfhm.png" },
-              { id: "s9", name: "Neura Brain Clarity ", price: "", variant: "Selling Fast", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875195038-to04osm0.png" },
-              { id: "s10", name: "Brain's Potential", price: "", variant: "Selling Fast", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875616012-9820ckf7.png" },
+              { id: "s1", name: "Memory Fuel", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777868743731-y4wa2vn3.png?_wi=1" },
+              { id: "s2", name: "Mind Armor", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777869055586-ikhqg5on.png?_wi=1" },
+              { id: "s3", name: "ZenCortex", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777869239141-ymqry49a.png?_wi=1" },
+              { id: "s4", name: "Java Brain", price: "", variant: "New Product", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777876557877-sxo8ck2a.png" },
+              { id: "s5", name: "Brain Song", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777873454182-yswjwhig.png" },
+              { id: "s6", name: "Brain's Potential", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875638563-frz4xls5.png" },
+              { id: "s7", name: "Neuro Prime", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777874864346-8ojlmuxm.png" },
+              { id: "s8", name: "Pineal Guardian X", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777874884898-6h39qfhm.png" },
+              { id: "s9", name: "Neura Brain Clarity ", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875195038-to04osm0.png" },
+              { id: "s10", name: "Brain's Potential", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875616012-9820ckf7.png" },
             ]}
           />
         </div>
