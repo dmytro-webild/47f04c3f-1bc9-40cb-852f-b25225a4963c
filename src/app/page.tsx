04efd7acde3fb0b88b9711c4022c39589576c4b9
bender <kudin.dima@gmail.com>
2026-05-04 05:26:26 +0000

    Switch to version 3: modified src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,7 +89,7 @@ export default function LandingPage() {
       columns={[
         { title: "Company", items: [{ label: "About", href: "/about" }, { label: "Blog", href: "/blog" }] },
         { title: "Products", items: [{ label: "Supplements", href: "/products" }, { label: "Resources", href: "#" }] },
-        { title: "Legal", items: [{ label: "Privacy Policy", href: "/privacy" }, { label: "Terms of Use", href: "#" }] },
+        { title: "Legal", items: [{ label: "Privacy Policy", href: "#" }, { label: "Terms of Use", href: "#" }] },
       ]}
     />
   </div>
