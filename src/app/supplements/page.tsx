b2c3f55e7b0536cbdc6f624c4dfc23222b876922
bender <kudin.dima@gmail.com>
2026-05-17 23:17:08 +0000

    Update src/app/supplements/page.tsx

--- src/app/supplements/page.tsx
+++ src/app/supplements/page.tsx
@@ -7,6 +7,19 @@ import FooterBaseCard from '@/components/sections/footer/FooterBaseCard';
 import ProductCardFour from '@/components/sections/product/ProductCardFour';
 
 export default function SupplementsPage() {
+  const supplements = [
+    { id: "s1", name: "Memory Fuel", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777868743731-y4wa2vn3.png?_wi=1", onProductClick: () => window.open('https://ab371lcl7z184n6ci71c-72dw5.hop.clickbank.net', '_blank') },
+    { id: "s2", name: "Mind Armor", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777869055586-ikhqg5on.png?_wi=1", onProductClick: () => window.open('https://www.lifecoredynamics.com/mindarmor?&shield=84a7bhfn-82f2y41pn5g6wdw8s', '_blank') },
+    { id: "s3", name: "ZenCortex", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777869239141-ymqry49a.png?_wi=1", onProductClick: () => window.open('https://fea986bo9d-43o8ti7r8ueooez.hop.clickbank.net', '_blank') },
+    { id: "s4", name: "Java Brain", price: "", variant: "New Product", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777876557877-sxo8ck2a.png", onProductClick: () => window.open('https://java-brain.link', '_blank') },
+    { id: "s5", name: "Brain Song", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777873454182-yswjwhig.png", onProductClick: () => window.open('https://brain-song.link', '_blank') },
+    { id: "s6", name: "NeuroXen", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875638563-frz4xls5.png", onProductClick: () => window.open('https://neuroxen.link', '_blank') },
+    { id: "s7", name: "Neuro Prime", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777874864346-8ojlmuxm.png", onProductClick: () => window.open('https://neuroprime.link', '_blank') },
+    { id: "s8", name: "Pineal Guardian X", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777874884898-6h39qfhm.png", onProductClick: () => window.open('https://pinealguardianx.link', '_blank') },
+    { id: "s9", name: "Neura Brain Clarity", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875195038-to04osm0.png", onProductClick: () => window.open('https://neurabrain.link', '_blank') },
+    { id: "s10", name: "NeuroXen", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875616012-9820ckf7.png", onProductClick: () => window.open('https://neuroxen.link', '_blank') },
+  ];
+
   return (
     <ThemeProvider
         defaultButtonVariant="bounce-effect"
@@ -41,18 +54,7 @@ export default function SupplementsPage() {
             gridVariant="uniform-all-items-equal"
             animationType="slide-up"
             useInvertedBackground={false}
-            products={[
-              { id: "s1", name: "Memory Fuel", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777868743731-y4wa2vn3.png?_wi=1" },
-              { id: "s2", name: "Mind Armor", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777869055586-ikhqg5on.png?_wi=1" },
-              { id: "s3", name: "ZenCortex", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777869239141-ymqry49a.png?_wi=1" },
-              { id: "s4", name: "Java Brain", price: "", variant: "New Product", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777876557877-sxo8ck2a.png" },
-              { id: "s5", name: "Brain Song", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777873454182-yswjwhig.png" },
-              { id: "s6", name: "NeuroXen", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875638563-frz4xls5.png" },
-              { id: "s7", name: "Neuro Prime", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777874864346-8ojlmuxm.png" },
-              { id: "s8", name: "Pineal Guardian X", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777874884898-6h39qfhm.png" },
-              { id: "s9", name: "Neura Brain Clarity ", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875195038-to04osm0.png" },
-              { id: "s10", name: "NeuroXen", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777875616012-9820ckf7.png" },
-            ]}
+            products={supplements}
           />
         </div>
 
