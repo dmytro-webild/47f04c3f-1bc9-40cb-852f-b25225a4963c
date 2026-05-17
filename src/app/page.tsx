37bf8a323196ea07902b002967a2924c8698ed1e
bender <kudin.dima@gmail.com>
2026-05-17 23:05:36 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function LandingPage() {
       gridVariant="uniform-all-items-equal"
       useInvertedBackground={true}
       products={[
-        { id: "p1", name: "Memory Fuel", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777876141937-p5z8u7iu.png", onProductClick: () => window.open('https://memoryfuel.example.com', '_blank') },
+        { id: "p1", name: "Memory Fuel", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777876141937-p5z8u7iu.png", onProductClick: () => window.open('https://ab371lcl7z184n6ci71c-72dw5.hop.clickbank.net', '_blank') },
         { id: "p2", name: "Mind Armor", price: "", variant: "Top Seller", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777876213411-e3c9sv8d.png", onProductClick: () => window.open('https://mindarmor.example.com', '_blank') },
         { id: "p3", name: "ZenCortex", price: "", variant: "New Product", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3CtJvMn9momZmpQncydBEDXlj8M/uploaded-1777876736631-lwi3bg5h.png", onProductClick: () => window.open('https://zencortex.example.com', '_blank') },
       ]}
